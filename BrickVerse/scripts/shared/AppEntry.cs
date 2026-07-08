ec17b30fefd6d9126ef61f06ef26b7ebaa778288
vq9o <53842001+vq9o@users.noreply.github.com>
2026-07-07 18:12:48 -0700

    Fix app entry

--- BrickVerse/scripts/shared/AppEntry.cs
+++ BrickVerse/scripts/shared/AppEntry.cs
@@ -2,16 +2,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+using System;
 using System.Collections.Generic;
 using BrickVerse.Client;
 using BrickVerse.DocsGen;
 using Godot;
 using static BrickVerse.Shared.Globals;
+
 #if DEBUG
-using System;
 using BrickVerse.DatamodelTest;
 #endif
-
 
 namespace BrickVerse.Shared;
 
